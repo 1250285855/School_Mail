621ce75fba2eb917db30df38118dac5e186f1278
沈逸帆 <1250285855@qq.com>
2022-10-26 12:04:14 +0800

    first commit

---
+++ frontend/src/School_Mail.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import Header from './views/Header.vue';
+import News from './views/News.vue';
+
+</script>
+
+<template>
+    <header>
+        <template>
+            <Header />
+        </template>
+    </header>
+
+    <News>
+        <template>
+            <News />
+        </template>
+    </News>
+
+</template>
