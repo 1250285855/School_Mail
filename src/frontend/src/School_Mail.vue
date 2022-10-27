5139ea8dda46309de1141889657e468dc2759c09
沈逸帆 <1250285855@qq.com>
2022-10-27 17:03:15 +0800

    1027更新

--- src/frontend/src/School_Mail.vue
+++ src/frontend/src/School_Mail.vue
@@ -2,20 +2,39 @@
 
 import Header from './views/Header.vue';
 import News from './views/News.vue';
+import Body from './views/Body.vue';
+import Footer from './views/Footer.vue';
 
 </script>
 
 <template>
-    <header>
-        <template>
-            <Header />
-        </template>
-    </header>
 
-    <News>
-        <template>
-            <News />
-        </template>
-    </News>
+  <div id="app">
+    <Header id="header"></Header>
+    <News id="news"></News>
+    <Body id="body"></Body>
+    <Footer id="footer"></Footer>
+  </div>
 
 </template>
+
+<!-- style 加上scoped标签，只会影响当前组件的css -->
+<style scoped>
+
+#app {
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+}
+
+#header {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100px;
+  background-color: rgb(200, 200, 200);
+}
+
+</style>
