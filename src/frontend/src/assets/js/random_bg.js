101ab98e8327038f998d57fc95360a906e383d42
沈逸帆 <1250285855@qq.com>
2022-10-30 18:36:46 +0800

    随机背景图实现

--- src/frontend/src/assets/js/random_bg.js
+++ src/frontend/src/assets/js/random_bg.js
@@ -3,15 +3,10 @@ import { ref } from 'vue'
 // require 是webpack的语法，vite不支持
 // const bg1 = require('@img/bg/bg1.jpg')
 
-const bg1 = new URL('@img/bg/bg1.jpg', import.meta.url)
-const bg2 = new URL('@img/bg/bg2.jpg', import.meta.url)
-const bg3 = new URL('@img/bg/bg3.jpg', import.meta.url)
-const bg4 = new URL('@img/bg/bg4.jpg', import.meta.url)
-const bg5 = new URL('@img/bg/bg5.jpg', import.meta.url)
+const num = Math.floor(Math.random()*10%5)
 
-const bgList = [bg1, bg2, bg3, bg4, bg5]
+const name = ref('bg'+num+'.jpg')
 
-export function randomBg() {
-    const bg = ref(bgList[Math.floor(Math.random() * bgList.length)])
-    return { bg }
+export function getRandomBg() {
+    return new URL(`../imgs/bg/${name.value}`, import.meta.url).href
 }
