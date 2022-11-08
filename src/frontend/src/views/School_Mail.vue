d67cd952eb781e218782cf64c636426a38bf4602
1300921244 <1300921244@qq.com>
2022-11-08 15:05:37 +0800

    此次更新：添加了关于我们页面组件

--- src/frontend/src/views/School_Mail.vue
+++ src/frontend/src/views/School_Mail.vue
@@ -8,6 +8,8 @@ import router from '@/router/index.js'
 
 import Header_skip from './components/header/Header_skip.vue';
 import Footer from '@/views/components/footer.vue'
+import Introduction from '@/views/components/introduction.vue';
+import AboutUs from '@/views/components/AboutUs.vue'
 
 const isOpen = ref(0)
 
@@ -26,6 +28,8 @@ const isOpen = ref(0)
     </div>
     
     <Footer></Footer>
+    <!-- <Introduction></Introduction> -->
+    <!-- <AboutUs></AboutUs> -->
 
   </div>
 
