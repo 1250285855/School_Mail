75caa17ac98271dc4d6f940ca2beff83d6505592
1300921244 <1300921244@qq.com>
2022-12-01 17:30:46 +0800

    班级风光组件更新

---
+++ src/frontend/src/views/components/班级风光.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="main">
+        <div class="Box1">
+            <div class="Text">
+                <p>计算机科学与技术专业实践</p>
+            </div>
+            <div class="Photo"></div>
+        </div>
+        <div class="Box2">
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.main .Box1 {
+    height: 600px;
+    background-color: rgba(20, 42, 49);
+    display: flex;
+    justify-content: space-around;
+}
+.main .Box1 .Text {
+    height: 600px;
+    width: 650px;
+    /* background-color: antiquewhite; */
+}
+.main .Box1 .Text p {
+    text-align: center;
+    font-size: 28px;
+    margin-top: 40px;
+    color: white;
+}
+.main .Box1 .Photo {
+    height: 500px;
+    width: 650px;
+    margin-top: 50px;
+    border-radius: 15px;
+    background-color: aliceblue;
+}
+.main .Box2 {
+    height: 600px;
+    background-color: aliceblue;
+}
+</style>
