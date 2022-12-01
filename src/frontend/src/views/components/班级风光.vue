81b163d4608c86fa96cd72757a2516f57a47aefc
1300921244 <1300921244@qq.com>
2022-12-01 18:00:32 +0800

    班级风光页面更新

--- src/frontend/src/views/components/班级风光.vue
+++ src/frontend/src/views/components/班级风光.vue
@@ -6,7 +6,12 @@
     <div class="main">
         <div class="Box1">
             <div class="Text">
-                <p>计算机科学与技术专业实践</p>
+                <div class="title">
+                    <p>计算机科学与技术专业实践</p>
+                </div>
+                <div class="Doc">
+
+                </div>
             </div>
             <div class="Photo"></div>
         </div>
@@ -26,13 +31,20 @@
 .main .Box1 .Text {
     height: 600px;
     width: 650px;
-    /* background-color: antiquewhite; */
+    background-color: antiquewhite;
+}
+.main .Box1 .Text .title {
+    float: left;
+    width: 650px;
 }
 .main .Box1 .Text p {
     text-align: center;
     font-size: 28px;
     margin-top: 40px;
     color: white;
+}
+.main .Box1 .Text .Doc {
+
 }
 .main .Box1 .Photo {
     height: 500px;
