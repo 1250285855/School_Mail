7741a8e34928ced917ce451f2f5d5bed887213e8
沈逸帆 <1250285855@qq.com>
2023-02-15 23:19:40 +0800

    修改header

--- src/frontend/src/views/components/header/Header_skip.vue
+++ src/frontend/src/views/components/header/Header_skip.vue
@@ -90,6 +90,10 @@ profile()
 </template>
 
 <style scoped>
+/*  Header配色方案1 20230214
+background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898;
+ background-blend-mode: multiply,multiply; */
+
  * {
     margin: 0;
     padding: 0;
